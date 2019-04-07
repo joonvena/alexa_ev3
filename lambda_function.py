4c93d87cc58ea17ee3b5449ab55fce07e67c60cb
Joonas Venäläinen <joonvena@gmail.com>
2019-04-07 12:56:23 +0300

    Updated file

---
+++ lambda_function.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+	request = event['request']
+
+	if request['type'] == 'IntentRequest':
+		intent = request['intent']
+		if intent['name'] == 'MoveBotForward':
+			move_bot_forward(intent)
+			return build_response('Moving bot to target position')
+
+		elif intent['name'] == 'AMAZON.FallbackIntent':
+			return build_reponse('I didn\'t understand that')
+		elif intent['name'] == 'AMAZON.HelpIntent':
+			return build_response('Ask me to move bot forward')
+		elif intent['name'] == 'AMAZON.CancelIntent' or intent['name'] == 'AMAZON.StopIntent':
+			return build_response('Goodbye')
+			
+def build_response(text):
+	return {
+		'version': '1.0',
+		'response': {
+			'outputSpeech': {
+				'type': 'PlainText',
+				'text': text
+			},
+			'shouldEndSession': True
+		}
+		}
+
+def move_bot_forward(intent):
+	request = 'move_bot_forward'
+	boto3.client('sqs').send_message(QueueUrl='https://sqs.eu-west-1.amazonaws.com/229803897151/ev3', MessageBody=request)
